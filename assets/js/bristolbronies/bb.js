18f1dcf957ff4f93eac8d11586c86f86e57d7fb3
Grey Hargreaves <shaun.hargreaves@gmail.com>
2015-03-31 08:30:07 +0100

    Functional/user friendliness improvements

--- assets/js/bristolbronies/bb.js
+++ assets/js/bristolbronies/bb.js
@@ -1,30 +1,40 @@
 var bb = {
-	log: function(component, message) {
+	log: function(component, message, color) {
+		color = typeof color != "undefined" ? color : "#e02437";
 		var formattedDate = "",
 		    date = new Date();
 		formattedDate += date.getFullYear() + "-" + ("0" + (date.getMonth()+1)).slice(-2) + "-" + ("0" + date.getMonth()).slice(-2);
 		formattedDate += " " + ("0" + date.getHours()).slice(-2) + ":" + ("0" + date.getMinutes()).slice(-2) + ":" + ("0" + date.getSeconds()).slice(-2);
-		console.log("[" + formattedDate + "]%c " + component, "color: #e02437", message);
+		console.log("[" + formattedDate + "]%c " + component, "color: " + color, message);
 	}
 };
 
+var hello = "/screens/welcome/",
+    goodbye = "/screens/goodbye/",
+    quiz = "/screens/quiz/",
+    quiza = "/screens/quiz/answers.html",
+    ponydrome = "https://www.youtube.com/embed/videoseries?list=PL5oyPmRlu5uqI2fyqxDfnGuYmWSJqDCbS";
+
 bb.stream = {
 	init: function() {
-		bb.stream.url("/screens/welcome/");
+		bb.stream.url(hello);
 	},
 	url: function(url) {
 		$(".js-streamer").attr("src", url);
 		bb.log("Streamer", "Changed frame to " + url);
+	},
+	toggle: function() {
+		$(".stream").toggleClass("stream--collapse");
 	}
 };
 
 bb.tweetstream = {
 	loadedTweets: [],
 	init: function() {
-		bb.tweetstream.checkForUpdate();
-		setInterval(bb.tweetstream.checkForUpdate, 30000); // 30 seconds
+		bb.tweetstream.update();
+		setInterval(bb.tweetstream.update, 30000); // 30 seconds
 	},
-	checkForUpdate: function() {
+	update: function() {
 		var template = $("#tmpl-tweet").html();
 		bb.log("Tweetstream", "Checking for updates...");
 		Mustache.parse(template);
@@ -50,7 +60,7 @@ bb.tweetstream = {
 			$tweetstream.prepend($(rendered).hide().fadeIn());
 			$tweetstream.children().each(function(index, element) {
 				var $element = $(element);
-				if(index > 4) { // max out at 5
+				if(index > 9) { // max out at 5
 					$element.fadeOut(500, function() { $element.remove(); });
 				}
 				else {
@@ -66,10 +76,10 @@ bb.tweetstream = {
 bb.schedule = {
 	lastTimestamp: 0000000000,
 	init: function() {
-		bb.schedule.checkForUpdate();
-		setInterval(bb.schedule.checkForUpdate, 62000); // 62 seconds
+		bb.schedule.update();
+		setInterval(bb.schedule.update, 62000); // 62 seconds
 	},
-	checkForUpdate: function() {
+	update: function() {
 		var template = $("#tmpl-schedule").html(),
 		    timeNow = Math.round(new Date().getTime()/1000);
 		bb.log("Schedule", "Checking for updates...");
@@ -108,3 +118,16 @@ $(document).ready(function() {
 	bb.tweetstream.init();
 	bb.stream.init();
 });
+
+// Handy documentation
+var help = function() {
+	bb.log("bb.stream.url(url)", "Navigate to a custom stream URL or enter a keyword.", "#0000cc");
+	bb.log("	hello", "Welcome message.", "#0000cc");
+	bb.log("	goodbye", "Goodbye message.", "#0000cc");
+	bb.log("	quiz", "Quiz question slides.", "#0000cc");
+	bb.log("	quiza", "Quiz answer slides.", "#0000cc");
+	bb.log("	ponydrome", "Ponydrome playlist (YouTube).", "#0000cc");
+	bb.log("bb.stream.toggle()", "Expand/collapse streamer (shows more tweets).", "#0000cc");
+	bb.log("bb.tweetstream.update()", "Force update tweetstream.", "#0000cc");
+	bb.log("bb.schedule.update()", "Force update schedule.", "#0000cc");
+};
