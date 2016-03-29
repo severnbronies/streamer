406f785a76eba555a4eb90cd69e050085623167e
chao-master <ripppony@gmail.com>
2016-03-29 23:16:15 +0100

    trying a slight bit of formatting

--- config-example.js
+++ config-example.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 	port: 9001,
-	adminPassword: "broniesYo"
+	adminPassword: "broniesYo",
 	twitterSearch: "severn bronies,severnbronies,6severn",
 	twitterId: 730239216, // @severnbronies
 	twitterAccessKey: "YOUR ACCESS KEY HERE",
