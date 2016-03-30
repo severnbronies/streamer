ef3fc955bf2a640897945d2377725df010e270c8
Kimberly Grey <kimandthemachine@gmail.com>
2016-03-30 19:57:59 +0100

    Refactor gulpfile, update JS libraries

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,48 +6,33 @@ var newer = require("gulp-newer");
 var imagemin = require("gulp-imagemin");
 
 gulp.task("default", function() {
-	gulp.watch("./assets/scss/{,*/}*.scss", ["stylesheets"]);
-	gulp.watch("./assets/js/vendor/{,*/}*.js", ["scripts-vendor"]);
-	gulp.watch("./assets/js/bristolbronies/{,*/}*.js", ["scripts-bristolbronies"]);
+	gulp.watch("./src/scss/{,*/}*.scss", ["stylesheets"]);
+	gulp.watch("./src/js/{,*/}*.js", ["scripts"]);
 });
 
+gulp.task("force", ["stylesheets", "scripts", "images"]);
+
 gulp.task("stylesheets", function() {
-	gulp.src("./assets/scss/bristolbronies.scss")
+	gulp.src("./src/scss/stylesheet.scss")
 	.pipe(sass(
 		{
 			errLogToConsole: true,
 			outputStyle: "compressed"
 		}
 	))
-	.pipe(gulp.dest("./assets/css"))
+	.pipe(gulp.dest("./dst/css"))
 });
 
-gulp.task("scripts-vendor", function() {
-	gulp.src(
-		[
-			"./assets/js/vendor/jquery-1.11.2.min.js",
-			"./assets/js/vendor/*.js"
-		]
-	)
+gulp.task("scripts", function() {
+	gulp.src("./src/js/vendor/{,*/}*.js")
 	.pipe(uglify())
-	.pipe(concat("vendor.min.js"))
-	.pipe(gulp.dest("./assets/js"))
-});
-
-gulp.task("scripts-bristolbronies", function() {
-	gulp.src(
-		[
-			"./assets/js/bristolbronies/bb.js",
-			"./assets/js/bristolbronies/*.js"
-		]
-	)
-	.pipe(uglify())
-	.pipe(concat("bristolbronies.min.js"))
-	.pipe(gulp.dest("./assets/js"))
+	.pipe(concat("scripts.js"))
+	.pipe(gulp.dest("./dst/js"))
 });
 
 gulp.task("images", function() {
-	gulp.src(["./assets/images/*", "!./assets/images/*.svg"])
+	gulp.src("./src/images/{,*/}*")
+	.pipe(newer("./dst/images"))
 	.pipe(imagemin(
 		{
 			optimizationLevel: 5,
@@ -56,5 +41,5 @@ gulp.task("images", function() {
 			multipass: true // svg
 		}
 	))
-	.pipe(gulp.dest("./assets/images"))
+	.pipe(gulp.dest("./dst/images"))
 });
